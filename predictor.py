2fefbb08403cf4d05988268329117f7012f55329
Xmen143 <83784820+Xmen143@users.noreply.github.com>
2021-08-26 21:26:26 +0530

    Add files via upload

---
+++ predictor.py
@@ -0,0 +1,10 @@
+### Custom definitions and classes if any ###
+import pandas as pd
+import matplotlib.pyplot as plt
+import sclearn
+import seaborn
+import numpy as np
+def predictRuns(testInput):
+    prediction = 0
+    ### code here ###
+    return prediction
